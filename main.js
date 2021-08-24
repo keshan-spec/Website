70a746fc376b78721c23f0f41e89320a6e3fe8db
keshan-spec <keshanth.programmer@gmail.com>
2021-08-24 13:11:30 +0530

    Fixed navbar responsiveness

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ const btn = document.getElementById("ham")
 const navLinks = document.getElementById("links")
 
 
+const clickHandler = () => {
+    if (navLinks.classList.contains("active")) {
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        document.body.style.position = 'fixed';
+    }
+    else {
+        btn.innerHTML = '<i class="fas fa-bars"></i>'
+        document.body.style.position = '';
+    }
+}
+
 btn.addEventListener("click", (e) => {
     e.preventDefault()
     navLinks.classList.toggle("active")
 
-    if (navLinks.classList.contains("active")) {
-        btn.innerHTML = '<i class="fas fa-times"></i>'
-    }
-    else {
-        btn.innerHTML = '<i class="fas fa-bars"></i>'
-    }
+    clickHandler()
 })
+
+navLinks.children.item(0).addEventListener("click", () => {
+    btn.innerHTML = '<i class="fas fa-bars"></i>'
+    document.body.style.position = '';
+    navLinks.classList.toggle("active")
+
+});
